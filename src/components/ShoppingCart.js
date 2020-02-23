9659b901b645d67b4016273e4c58e0506c442381
duongnguyen <duongnv.05@gmail.com>
2020-02-23 22:31:54 +0700

    styling empty cart page

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -3,9 +3,25 @@ import { connect } from 'react-redux';
 
 class ShoppingCart extends Component {
   render() {
+    const { shoppingCart } = this.props;
     return (
       <div className="_shopping-cart _content">
-        <h1>Shopping Cart</h1>
+        <div className="wrapper">
+          { shoppingCart.length < 1 ?
+            /* empty cart */
+            (
+              <div className="notify-empty">
+                <img className="img-empty-cart" src={require('../images/shopping-cart/empty.png')} alt="empty cart" />
+                <p className="message">You don't have any products in cart</p>
+              </div>
+            ) : (
+              <div className="shopping-cart-info">
+                <h1>You have product(s)</h1>
+              </div>
+            )
+            /* shopping cart */
+          }
+        </div>
       </div>
     )
   }
